ec434ffe164317bff747ab50e48edd85e5786ae3
HellsDefender <martinyquentin@yahoo.fr>
2018-05-22 16:59:34 +0200

    Header changer et Medias head
    MaJ CSS et Head.php

---
+++ parc d'activité/head.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scal=1, shrink-to-fit=no">
+	<title>Parc activité Joigny</title>
+
+	<link rel="stylesheet" href="assets/css/owl.carousel.css">
+	<link rel="stylesheet" href="assets/css/owl.theme.default.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/fontawesome-all.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="assets/js/jquery.timelinr-0.9.7.js" type="text/javascript"></script>
+	<script src="assets/js/bootstrap.js" type="text/javascript"></script>
+	<script src="assets/js/owl.carousel.js"></script>
+	<script src="assets/js/script.js" type="text/javascript"></script>
+</head>
+<body>
+	<header class="container-fluid">
+		<section class="container" id="imgH">
+			<div class="row">
+				<div class="col-md-2 py-4 mb-4">
+					<a href="accueil.png"><img src="assets/img/logo.png" id="logoS"></a>
+				</div>
+				<div class="col-md-8 py-4 mb-4 mt-4" id="bgB">
+					<h1 id="title">Parc d'activités d'Appoigny</h1>
+				</div>
+			</div>
+		</section>
+	</header>
+	<section class="container">
+		<div class="row">
+			<div class="col-md-9 offset-md-2 col-12">
+				<nav class="navbar navbar-expand navbar-transparent bg-transparent">
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<ul class="navbar-nav">
+						<li class="nav-item active">
+							<a class="nav-link" href="accueil.php"><img src="assets/img/sketching.png" class=" pictogramme"><p class="petitT">Le projet</p><span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="chantier.php"><img src="assets/img/dump-truck.png" class=" pictogramme"><p class="petitT">Le chantier</p></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="multimedia.php"><img src="assets/img/photo-camera.png" class=" pictogramme"><p class="petitT">Photos/Vidéos</p></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="actualité.php"><img src="assets/img/newspaper.png" class="pictogramme"><p class="petitT">Actualités</p></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="contacts.php"><img src="assets/img/phone-book.png" class=" pictogramme"><p class="petitT">Contact</p></a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</div>
+</section>
