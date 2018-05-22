09bc0d6791f8ead1ee17c202189eb207303c4d72
1kidam1 <yanngde@hotmail.fr>
2018-05-22 16:34:51 +0200

    deux ou trois choses en responsive

---
+++ parc d'activité/assets/chantier.php
@@ -0,0 +1,32 @@
+<?php 
+include_once('header.php');
+ ?>
+	<main class="container-fluid">
+		<img src="assets/img/digger.png" class="forest d-none d-md-block">
+		<img src="assets/img/bulldozer.png" class="build d-none d-md-block">
+		<section class="container">
+			<div class="col-12">
+				<h2>Le chantier en chiffres :</h2>
+				<p>• Terrassements : 200 000 m3 de déblais ;<br> • 5 bassins de stockage & temporisation des eaux pluviales ; <br>
+				• Voirie neuve : 2 km en tranche ferme et 500 m en tranche optionnelle ; <br>• 2 giratoires de raccordement, à la RN6 au sud et à la RD319 au nord ; <br>• Mobilités : 2 arrêts de bus, cheminements piétons et pistes cyclables, 20 emplacements de recharge pour véhicules électriques ;<br>• 1 parking poids lourds et 10 îlots pour stationnements VL visiteurs, sur espaces publics <br> • 3 200 arbres et arbustes pré-plantés ; <br>• Budget d’opération : 30 M€ toutes dépenses confondues.</p>
+				<img src="assets/img/planning.png" class="img-fluid" alt="Responsive image">
+				
+			</div>
+			<div class="col-12">
+				<img src="assets/img/plan.png" class="img-fluid" alt="Responsive image">
+				<p><strong>Cliquez ici pour voir la legende.</strong> </p>
+			</div>
+			<label class="switch">
+
+				<input type="checkbox" id="submit">
+				<span class="slider round"></span>
+
+			</label>			
+			<dir class="col-12" id="feedback">
+				<img src="assets/img/legende.png" class="img-fluid" alt="Responsive image">
+			</dir>
+		</section>
+	</main>
+<?php 
+include_once('footer.php');
+ ?>
